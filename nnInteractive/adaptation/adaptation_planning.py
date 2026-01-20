Pushing some modifications to fix the auto re-run, and also moving the intensity normalisation to the dataloader

nnInteractive/adaptation/adaptation_planning.py
@@ -201,8 +201,8 @@ class Prototype_Static_Planner:
             #available VRAM/availability of samples.  E.g., using mini-batch to introduce stochasticity/regularising
             #effects for small sample sizes.
             'total_iterations': total_iterations, 
-            'num_workers': 1,
-            'pin_memory': True,
+            'num_workers': 0,
+            'pin_memory': False,
             'deterministic_transforms':{
                 'LoadImaged': {
                     'keys': ['image', 'label'],
@@ -226,13 +226,15 @@ class Prototype_Static_Planner:
                     'allow_missing_keys': False,
                     'channel_dim': None
                     },
-                'ToDeviced': {
+                'EnsureTyped': {
                     'keys': ['image', 'label'],
+                    'data_type': "tensor",
+                    'dtype': [torch.float32, torch.int8],
                     'device': 'cuda',
-                    'non_blocking': True,
+                    'wrap_sequence': True,
+                    'track_meta': None,
                     'allow_missing_keys': False,
-                    'lazy': False,
-                    },     
+                    },   
             },
             'dynamic_transforms':{
                 'RandCropByPosNegLabeld': {
@@ -251,11 +253,14 @@ class Prototype_Static_Planner:
                     'bg_indices_key': None,
                     },
                 'NormalizeIntensityd': {
-                    'keys': ['image', 'label'],
+                    'keys': ['image'],
                     'subtrahend': None,
                     'divisor': None,
+                    'nonzero': False,
                     'channel_wise': False,
-                    'dtype': torch.float32
+                    'dtype': torch.float32,
+                    'allow_missing_keys': False,
+
                 },      
                 'DivisiblePadd': {
                     'keys': ['image', 'label'],
@@ -293,9 +298,9 @@ class Prototype_Static_Planner:
             #available VRAM/availability of samples. And patch-subsampling if used will also have an effect. We want
             #a reasonable measure of performance, which can suffer if samples are correlated (subsampling) or
             #batch size is too small.
-            'num_workers': 1,
+            'num_workers': 0,
             'total_iterations': total_iterations,
-            'pin_memory': True,
+            'pin_memory': False,
             'metrics': True,
             'deterministic_transforms':{
                 'LoadImaged': {
@@ -320,12 +325,14 @@ class Prototype_Static_Planner:
                     'allow_missing_keys': False,
                     'channel_dim': None
                     },
-                'ToDeviced': {
+                'EnsureTyped': {
                     'keys': ['image', 'label'],
+                    'data_type': "tensor",
+                    'dtype': [torch.float32, torch.int8],
                     'device': 'cuda',
-                    'non_blocking': True,
+                    'wrap_sequence': True,
+                    'track_meta': None,
                     'allow_missing_keys': False,
-                    'lazy': False,
                     },
             },
             'dynamic_transforms':{
@@ -349,11 +356,13 @@ class Prototype_Static_Planner:
                     'lazy': False,
                     }, 
                 'NormalizeIntensityd': {
-                    'keys': ['image', 'label'],
+                    'keys': ['image'],
                     'subtrahend': None,
                     'divisor': None,
+                    'nonzero': False,
                     'channel_wise': False,
-                    'dtype': torch.float32
+                    'dtype': torch.float32,
+                    'allow_missing_keys': False
                 },
                 'DivisiblePadd': {
                     'keys': ['image', 'label'],

nnInteractive/adaptation/training_utils/trainer.py
@@ -141,7 +141,7 @@ class Trainer:
             self.input_buffer[:,0:1,:,:,:] = input_image
             #Never mind, why the hell am i normalising here? This should be done in the dataloader so that
             #we can have a training pipeline which will actually allow for augmentation properly. 
-            
+
             # #Normalise!!! DO NOT FORGET TO NORMALISE THE IMAGE INPUT!!! Can't believe we forgot to do this...
             # self.input_buffer[:,0:1,:,:,:] -= self.input_buffer[:,0:1,:,:,:].mean()
             # self.input_buffer[:,0:1,:,:,:] /= self.input_buffer[:,0:1,:,:,:].std()
@@ -166,6 +166,7 @@ class Trainer:
 
         for batch_idx in range(self.train_dataloading_config['total_iterations']):
             batch_data = next(iter(self.train_dataloader))
+            batch_data = {k: v.cpu() for k, v in batch_data.items()}
             metric_dict[batch_idx] = dict() 
 
             self.global_iter_step += 1
@@ -475,6 +476,8 @@ class Trainer:
         
         for batch_idx in range(self.val_dataloading_config['total_iterations']):
             batch_data = next(iter(self.val_dataloader))
+            batch_data = {k: v.cpu() for k, v in batch_data.items()}
+            torch.cuda.empty_cache()
             metric_dict[batch_idx] = dict()    
             per_iter_metric_dict = dict() #Initialise the metric dict for this batch which stores per-iter metrics.
 